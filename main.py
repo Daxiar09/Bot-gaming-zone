bc54a96c3ff8243a11103299c408b8f9a4b912b2
Daxiar09 <leon.lechevallier12@gmail.com>
2025-06-04 11:56:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ui import Button, View
+import json
+import asyncio
+from dotenv import load_dotenv
+from keep_alive import keep_alive
+import re
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+await bot.load_extension("moderation")
