dc4244d1eeda6c9b9647c343f17d5a874d0e4c2d
Daxiar09 <leon.lechevallier12@gmail.com>
2025-06-05 08:22:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import os
 import discord
 from discord.ext import commands
-from discord import app_commands
-from discord.ui import Button, View
-import json
-import asyncio
 from dotenv import load_dotenv
 from keep_alive import keep_alive
-import re
+import asyncio
 
 keep_alive()
 
@@ -23,12 +19,15 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} est connecté !")
+    await bot.tree.sync()  # Synchronise les commandes slash avec Discord
+    print(f"{bot.user} est connecté ! Les commandes slash sont synchronisées.")
 
 
 async def main():
-    await bot.load_extension("moderation")
-    await bot.start(token)
+    async with bot:
+        await bot.load_extension("moderation"
+                                 )  # Charge ton fichier moderation.py
+        await bot.start(token)
 
 
 asyncio.run(main())
